d6351d6a81620190f259c5f5ce9e2a8b40a47db6
ftmzhrsckldr <36725373+ftmzhrsckldr@users.noreply.github.com>
2024-05-16 15:53:36 +0300

    BE-2844 Add compatibility for different Jira API versions (#1)

--- README.md
+++ README.md
@@ -5,12 +5,22 @@ Add comment to Jira Issue and optionally change the status.
 ## Required Inputs
 
 - `AC_JIRA_HOST`: Jira Host. Your Jira subdomain. Example: `mysubdomain.atlassian.net`
-- `AC_JIRA_EMAIL`: Jira Email. Email of Jira user. Please add this using **locked** environment variables.
-- `AC_JIRA_TOKEN`: API Token. User's API Token. Please add this using **locked** environment variables. You can create your token from [here](https://id.atlassian.com/manage-profile/security/api-tokens)
-- `AC_JIRA_ISSUE`: Jira Issue. The ID or key of the issue. Please check [documentation](https://docs.appcircle.io/integrations/jira-integration) to learn how you can get this information from branch names or commit messages.
-- `AC_JIRA_TEMPLATE`: Comment Template. This comment template will be used to post a comment. Variables donated with `$` will be replaced during the build.
+- `AC_JIRA_ISSUE`: Jira Issue. The ID or key of the issue. Please check [documentation](https://docs.appcircle.io/workflows/common-workflow-steps/jira-comment) to learn how you can get this information from branch names or commit messages.
+- `AC_JIRA_TEMPLATE`: Comment Template. This comment template will be used to post a comment. Variables donated with `$` will be replaced during the build. Refer to [documentation](https://docs.appcircle.io/workflows/common-workflow-steps/jira-comment#changing-template) to modify the template.
 
 ## Optional Inputs
 
-- `AC_JIRA_FAIL_TRANSITION`: Error Transition. Transition ID or name for the failed step. If the previous state succeeds, you can optionally change the status of your issue.
-- `AC_JIRA_SUCCESS_TRANSITION`: Success Transition. Transition ID or name for the successful step. If the previous state succeeds, you can optionally change the status of your issue.
+- `AC_JIRA_EMAIL`: Email of Jira user. This field is **required** for those using API tokens instead of PAT. Please add this using **locked** environment variables.
+- `AC_JIRA_TOKEN`: User's API Token. If this value is filled, the Jira e-mail field must also be filled. Only Jira Cloud users can use API token. You can create token from [here](https://id.atlassian.com/manage-profile/security/api-tokens).
+- `AC_JIRA_PAT`: Specify the Personal Access Token (PAT) for Jira authentication. Only Jira On-Prem users can use PAT.
+- `AC_JIRA_FAIL_TRANSITION`: Transition ID or name for the failed step. Optionally change the status of your issue if the previous state fails. Ensure that the "Always run this step even if the previous steps fail" switch is enabled for this feature to work.
+- `AC_JIRA_SUCCESS_TRANSITION`: Transition ID or name for the successful step. Optionally change the status of your issue if the previous state succeeds.
+
+## Contributing
+
+When contributing to the **Jira Comment** repository, it's important to note that the comment input types depend on the Jira REST API version. Therefore, the repository has different versions in the [Workflow Components](https://github.com/appcircleio/appcircle-workflow-components) repository:
+
+- component version `2.*.*` for Jira API version 2
+- component version `3.*.*` for Jira API version 3
+
+Be sure to select the appropriate component version based on the Jira REST API version you are working with. For more detailed information, refer to the [Jira Comment](https://docs.appcircle.io/workflows/common-workflow-steps/jira-comment#jira-rest-api-version-reference) documentation.
--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@
 require 'English'
 require 'net/http'
 require 'json'
+require 'base64'
 
 def env_has_key(key)
   value = ENV[key]
@@ -24,38 +25,84 @@ def get_env(key)
   end
 end
 
-def post(payload, endpoint, username, access_key, parse = true)
+def clean_commit_message(commit_message)
+  commit_message_without_line = commit_message.gsub("\n", "")
+  File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
+    f.puts "AC_COMMIT_MESSAGE=#{commit_message_without_line}"
+  end
+end
+
+
+def jira_auth(jira_token, username = nil)
+  if $jira_pat != nil
+    return "Bearer #{jira_token}"
+  else
+    if username == nil
+      abort("Missing Jira e-mail. If you will not use Personal Access Token as authorization, you must enter your Jira e-mail and API token.")
+    end
+    return "Basic #{Base64.strict_encode64("#{username}:#{jira_token}")}"
+  end
+end
+
+def handle_http_errors(response)
+  case response
+  when Net::HTTPClientError, Net::HTTPServerError
+    abort "\nError: #{response.code} #{response.message}\n#{response.body}\n\n"
+  end
+end
+
+def set_jira_api_version()
+  jira_template_v2 = get_env('AC_JIRA_TEMPLATE_V2')
+  jira_template_v3 = get_env('AC_JIRA_TEMPLATE_V3')
+
+  if !jira_template_v2.nil?
+    $input = jira_template_v2
+    $rest_api_version = 2
+  elsif !jira_template_v3.nil?
+    $input = jira_template_v3
+    $rest_api_version = 3
+  else
+    abort("Please fill in Comment Template (either the AC_JIRA_TEMPLATE_V2 or AC_JIRA_TEMPLATE_V3) variable to proceed.")
+  end
+end
+
+def post(payload, endpoint, jira_token, username = nil, parse = true)
   uri = URI.parse(endpoint)
   req = Net::HTTP::Post.new(uri.request_uri,
                             { 'Content-Type' => 'application/json' })
-  req.basic_auth(username, access_key)
+  req['Authorization'] = jira_auth(jira_token, username)
   req.body = payload
-  req.basic_auth(username, access_key)
   res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
     http.request(req)
   end
+  handle_http_errors(res)
   JSON.parse(res.body, symbolize_names: true) if parse
 end
 
-def get(endpoint, username, access_key)
+def get(endpoint, jira_token, username = nil)
   uri = URI.parse(endpoint)
   req = Net::HTTP::Get.new(uri.request_uri,
                            { 'Accept' => 'application/json' })
-  req.basic_auth(username, access_key)
+  req['Authorization'] = jira_auth(jira_token, username)
   res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
     http.request(req)
   end
+  handle_http_errors(res)
   JSON.parse(res.body, symbolize_names: true)
 end
 
-def create_payload(input, success)
+def create_payload(success)
   panel = success ? 'success' : 'error'
-  input = input.sub('AC_JIRA_PANEL', panel)
+  $input = $input.sub('AC_JIRA_PANEL', panel)
   time = Time.now.utc.strftime('%m/%d/%Y %H:%M:%S')
-  input = input.sub('AC_JIRA_DATE', time)
+  $input = $input.sub('AC_JIRA_DATE', time)
+
+  if $rest_api_version != 2
+    $input = JSON.parse($input)
+  end
 
   payload = {
-    body: JSON.parse(input)
+    body: $input
   }
   payload.to_json
 end
@@ -69,25 +116,33 @@ def transitionid(id, transitions)
 end
 
 jira_host = env_has_key('AC_JIRA_HOST')
-username = env_has_key('AC_JIRA_EMAIL')
-access_key = env_has_key('AC_JIRA_TOKEN')
+commit_message = env_has_key('AC_COMMIT_MESSAGE')
+clean_commit_message(commit_message)
+username = get_env('AC_JIRA_EMAIL')
+access_key = get_env('AC_JIRA_TOKEN')
+$jira_pat = get_env('AC_JIRA_PAT')
+jira_token = $jira_pat || access_key
+if jira_token.nil?
+  abort "Could not find Jira token or PAT for authentication."
+end
 issue_id = env_has_key('AC_JIRA_ISSUE')
 is_success = get_env('AC_IS_SUCCESS')
 success = is_success == 'true' || is_success == 'True'
 success_id = get_env('AC_JIRA_SUCCESS_TRANSITION')
 failure_id = get_env('AC_JIRA_FAIL_TRANSITION')
-input = env_has_key('AC_JIRA_TEMPLATE')
-endpoint = "#{jira_host}/rest/api/3/issue/#{issue_id}/comment"
-payload = create_payload(input, success)
+set_jira_api_version()
+endpoint = "#{jira_host}/rest/api/#{$rest_api_version}/issue/#{issue_id}"
+comment_endpoint = endpoint + "/comment"
+payload = create_payload(success)
 puts "Posting comment for #{issue_id}"
 $stdout.flush
-result = post(payload, endpoint, username, access_key)
+result = post(payload, comment_endpoint, jira_token, username, true)
 puts "Comment #{result[:id]} posted"
 $stdout.flush
 
 if success_id || failure_id
-  endpoint = "#{jira_host}/rest/api/3/issue/#{issue_id}/transitions"
-  result = get(endpoint, username, access_key)
+  transitions_endpoint = endpoint + "/transitions"
+  result = get(transitions_endpoint, jira_token, username)
   if success
     id = transitionid(success_id, result)
     puts 'Workflow succeeded'
@@ -98,8 +153,7 @@ if success_id || failure_id
   puts "Transition ID: #{id}"
   $stdout.flush
 
-  endpoint = "#{jira_host}/rest/api/3/issue/#{issue_id}/transitions"
   payload = "{\"transition\":{\"id\":\"#{id}\"}}"
-  post(payload, endpoint, username, access_key, false)
+  post(payload, transitions_endpoint, jira_token, username, false)
   puts "Issue transitioned to #{id}"
 end
